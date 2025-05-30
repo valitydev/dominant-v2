3c5c20ccd7585d630c1e5b02d3d555430306868d
Aleksey Kashapov <nanodirijabl@gmail.com>
2025-05-30 10:38:18 +0300

    Bumps deps

--- apps/dmt/test/dmt_integration_tests_SUITE.erl
+++ apps/dmt/test/dmt_integration_tests_SUITE.erl
@@ -226,6 +226,7 @@ insert_remove_referencing_object_success_test(Config) ->
             object =
                 {provider, #domain_Provider{
                     name = <<"name">>,
+                    realm = test,
                     description = <<"description">>,
                     proxy = #domain_Proxy{
                         ref = ProxyRef,
